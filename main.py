0f6f271ffdd01749a7269a646038e495042f2db3
newlysong <saron01@g.skku.edu>
2024-04-18 23:29:35 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def main():
+    # Example list
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    
+    even_int_list = even_list(int_list)    
+    output = sum_of_squares_of_even(even_int_list)    
+    print(output)
